85078057255fedcd0f89a63cd46a7e512b0caf57
Tyrone <ty@crayoncapsule.co.za>
2013-08-05 21:39:47 +0200

    Tyronestarts building the manage list editable table

--- htdocs/managelist.php
+++ htdocs/managelist.php
@@ -5,6 +5,8 @@
 $user = new User();
 $user->authUser($_SESSION['user_auth']);
 
+$mail = new Mailer();
+
 ?>
 
 <!DOCTYPE html>
@@ -26,18 +28,78 @@ $user->authUser($_SESSION['user_auth']);
             </nav>
         
         <div id="wrapper" class="clearfix">
-            
             <!--==================================== CONTENTS ===========-->
-            <div class="contents">
-                <div id="importlist" class="left">
-                    <form id="importlistform" enctype="multipart/form-data">
-                        <h2>Import List</h2>
-                        <input id="fileupload" type="file" />
-                        <input id="upload" type="button" value="Upload" />
-                    </form>
+            <div class="contents clearfix">
+                <div id="logs_menu_holder" class="left">
+                    <h2>Lists Archive</h2>
+                    <div id="logs_menu">
+                        <form method="post" action="">
+                            <select>
+                                <option>Please make a selection</option>
+                                <option>Clients 2013</option>
+                                <option>Year-End 2013</option>
+                                <option>Fishgate invitation 2013</option>
+                            </select>
+                            <br />
+                            <input type="button" value="View List Details">
+                        </form>
+                    </div>
+                </div>
+                
+                <div class="left managelist_holder">
+                    <h3>Comprehensive View</h3>
+                    <div class="managelist_table">
+                        <form method="post">
+                        <table border="1" width="100%" cellpadding="5">
+                            <tr>
+                                <td class="recent_heads">Action</td>
+                                <td class="recent_heads">To</td>
+                                <td class="recent_heads">Email</td>
+                                <td class="recent_heads">Date</td>
+                                <td class="recent_heads">Opened</td>
+                            </tr>
+                            
+                            <?php
+                            
+                            try {
+                                if($email_logs = $mail->outputLogs()){
+                                    foreach($email_logs as $log){
+                                        ?>
+                                        <tr>
+                                            <td>
+                                                <input type="checkbox" clas="remove_row" />
+                                                Remove
+                                            </td>
+                                            <td><?php echo $log['name']; ?></td>
+                                            <td><?php echo $log['email']; ?></td>
+                                            <td><?php echo $log['date']; ?></td>
+                                            <td align="center">
+                                                <?php 
+                                                if($log['opened']){
+                                                    echo '<span style="color: green;">&#x2713;</span>';
+                                                }else{
+                                                    echo '<span style="color: red;">&#x2717;</span>';
+                                                }
+                                                ?>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                    }
+                                }
+                            } catch (Exception $ex) {
+                                ?>
+                                <tr>
+                                    <td colspan="4"><?php echo $ex->getMessage(); ?></td>
+                                </tr>
+                                <?php
+                            }
+                            
+                            ?>
+                        </table>
+                        </form>
+                    </div>
                 </div>
             </div>
-            
         </div>    
     </body>
 </html>
